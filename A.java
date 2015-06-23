63e7333c3522a6d7f039768baae2b62c20bae159
Kei Hitomi <khitomi337@gmail.com>
2015-06-22 22:34:38 -0400

    New example added to A.java

--- A.java
+++ A.java
@@ -2,8 +2,10 @@ public class A{
 
   public static void main(String args[]){
     
-    System.out.println("Chris sucks dick");
-    
+    System.out.println("New Chris really sucks dick");
+
+    System.out.println("Other new stuff added");
+
   }
 
 }
