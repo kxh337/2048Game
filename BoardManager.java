a91e0a349f80ce2796b77383df8ad78522b57ba7
Kei Hitomi <khitomi337@gmail.com>
2015-06-23 22:37:12 -0400

    made some changes before dennis

--- BoardManager.java
+++ BoardManager.java
@@ -3,7 +3,9 @@ import javax.swing.JFrame;
 public class BoardManager{
 
   public void makeGUI(){
-    JFrame frame = new JFrame("2048 Game");
+    JFrame frame = new JFrame("This is wrong delete this line");
+    int yolo = 69;
+    String haha = "whoohoo";
     
   }
 
